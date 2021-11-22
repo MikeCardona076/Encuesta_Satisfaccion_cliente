374670f0b1156290637d101e6f52fe70d1d339e2
Mike Cardona <40007337+MikeCardona076@users.noreply.github.com>
2021-11-22 08:09:17 -0800

    Cambios Finales

--- macc_poll/settings.py
+++ macc_poll/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'macc_poll.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
